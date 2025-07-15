1592a2ac98b509c05861f50f1a329de595792a85
Sharlotte <nguyennhonhau25052003@gmail.com>
2025-07-15 16:46:55 +0700

    api versioning

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: 'v4',
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
 bootstrap();
